5f13c33da8bce00e6356bcb5256d217372d06368
Aaron Eiche <aeiche@jaguarlandrover.com>
2019-04-09 14:52:31 -0700

    Adds product request to remote API.

--- routes/search.js
+++ routes/search.js
@@ -12,19 +12,62 @@ router.get('/:searchTerm', async (req,res,next) => {
   12662817,34890820,19716431,42391766,35813552,40611708,
   40611825,36248492,44109840,23117408,35613901,42248076,
   
-
   //A request for 1/3 of the products.
-  http://api.walmartlabs.com/v1/items/?apiKey={apiKey}&ids=14225185,14225186,14225188,14225187,39082884,30146244
-  
+  http://api.walmartlabs.com/v1/items/?apiKey={apiKey}&ids=14225185,14225186,14225188,14225187...
+ 
   */
-  // const wmItemsURL = `http://api.walmartlabs.com/v1/items/?apiKey=${apiKey}`;
 
-  // await axios.get(``)
+  const products = [
+    "14225185",
+    "14225186",
+    "14225188",
+    "14225187",
+    "39082884",
+    "30146244",
+    "12662817",
+    // "34890820",
+    // "19716431",
+    // "42391766",
+    // "35813552",
+    // "40611708",
+    // "40611825",
+    // "36248492",
+    // "44109840",
+    // "23117408",
+    // "35613901",
+    // "42248076"
+  ];
 
-  data = {}
+  const maxItemLength = 6;  // Determined emperically.
+  var productResults = [];  // An array to feed products into.
+
+  while(products.length > 0) {
+    
+    let subset = products.splice(0,maxItemLength);
+
+    var url = `http://api.walmartlabs.com/v1/items?apiKey=${process.env.APIKEY}&ids=${subset.join(",")}`;
+    // console.log(url);
+    
+    let callProducts = await axios.get(url).then(function(response){
+      // console.log(response.status);
+      // console.log(response.data);
+      return response.data.items;
+      // console.log(typeof response.data.items);
+      // if(response.data.items) {
+      //   return response.data.items;
+      // }
+      // return [];
+    });
+    productResults = productResults.concat(callProducts);
+
+    
+    
+  }
+
+  // data = {}
   
   res.status(200); //Broadly assume we're finding something here.
-  res.json(data);
+  res.json(productResults);
 
 });
 
